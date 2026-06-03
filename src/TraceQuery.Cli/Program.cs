af65d289f3d629d7b3246d7fd03f3dd52f661666
Mate Kreko <mate.kreko@gmail.com>
2026-06-03 05:02:58 +0200

    feat(cli): add to print source content to console given a file-path argument

--- src/TraceQuery.Cli/Program.cs
+++ src/TraceQuery.Cli/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using TraceQuery.Core.Ingestion;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if ( 0 < args.Length )
+        {
+            String path = args[0];
+            String? lineBuffer;
+
+            try
+            {
+                using FileReader fileReader = new FileReader(path);
+
+                while ( null != ( lineBuffer = fileReader.RouteLine() ) )
+                {
+                    Console.WriteLine(lineBuffer);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
